b03a314709d2a0eda115af4c88fcca35a41c1ca1
right42 <chjm452@naver.com>
2021-05-21 23:38:05 +0900

    init

---
+++ src/main/java/me/right42/querydslpractice/QuerydslPracticeApplication.java
@@ -0,0 +1,13 @@
+package me.right42.querydslpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerydslPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/right42/querydslpractice/QuerydslPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.right42.querydslpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
